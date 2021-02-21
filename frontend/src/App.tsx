c31e8c43bafb998f2ebaffd25f37a7275329811b
Francesco Canessa - @makevoid <makevoid@gmail.com>
2021-02-21 22:49:46 +0000

    initial commit with bulma

---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Symfoni } from "./hardhat/SymfoniContext";
+import { Greeter } from './components/Greeter';
+import { Greeter2 } from './components/Greeter2';
+import { Section, Container, Hero, Heading } from 'react-bulma-components';
+
+function App() {
+
+  return (
+    <Container className="bordered">
+      <Hero color="primary">
+        <Hero.Body>
+          <Container>
+            <Heading>
+              Eth App
+            </Heading>
+          </Container>
+        </Hero.Body>
+      </Hero>
+
+      <Symfoni autoInit={true} >
+        <Section>
+          <Heading size={4}>
+            Greeter:
+          </Heading>
+          <Greeter></Greeter>
+          <Greeter2></Greeter2>
+        </Section>
+      </Symfoni>
+    </Container>
+  );
+}
+
+export default App;
